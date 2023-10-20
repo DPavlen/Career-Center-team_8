574ddf21b68449756bbb6fc336acc659c55d5a43
rastereo <kapturhouse@yandex.ru>
2023-10-20 19:16:11 +0300

    feat: create react app and add ys fonts

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="title">
+        Хакатон Карьерный трекер х Практикум октябрь’23
+      </h1>
+      <p>Группа №8</p>
+    </main>
+  );
+}
+
+export default App;
