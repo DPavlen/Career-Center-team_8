77a2e04a06baec83301f38f31954caf6abb92c33
rastereo <kapturhouse@yandex.ru>
2023-10-24 14:09:52 +0300

    refactor: create theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
+import './index.scss';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import { store } from './store/store';
 import App from './components/App/App.tsx';
-import './index.scss';
+import theme from './components/theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
