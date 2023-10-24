aaff1c067a5f07f9b8509f9246e7bd5d7e9b3158
cipher24 <mediumAndrew@yandex.ru>
2023-10-25 01:32:51 +0300

    fix conflict

--- frontend/src/pages/Candidates/Candidates.tsx
+++ frontend/src/pages/Candidates/Candidates.tsx
@@ -2,16 +2,19 @@ import './Candidates.scss';
 import Typography from '@mui/material/Typography';
 import AppliedFilters from '../../components/AppliedFilters/AppliedFilters';
 import VacanciesCards from '../../components/VacanciesCards/VacanciesCards';
-// import Filters from '../../components/Filters/Filters';
+import Filters from '../../components/Filters/Filters';
 
 function Candidates() {
   return (
-    <main style={{ paddingLeft: '32px' }}>
-      <Typography variant="h1">
-        Кандидаты
-      </Typography>
-      <AppliedFilters />
-      <VacanciesCards />
+    <main className="candidates" style={{ paddingLeft: '32px', display: 'grid', gridTemplateColumns: '1fr  minmax(auto, 382px)' }}>
+      <div>
+        <Typography variant="h1">
+          Кандидаты
+        </Typography>
+        <AppliedFilters />
+        <VacanciesCards />
+      </div>
+      <Filters />
     </main>
   );
 }
