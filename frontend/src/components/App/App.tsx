815a9a1af714260df39cb4df9d3adc0b56e6adeb
rastereo <kapturhouse@yandex.ru>
2023-10-27 23:35:18 +0300

    feat: create login account

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser, clearUser } from '../../store/user/user';
 import { addCandidates, clearCandidates } from '../../store/candidates/candidates';
@@ -10,31 +15,23 @@ import './App.scss';
 import mainApi from '../../utils/MainApi';
 
 import Sidebar from '../Sidebar/Sidebar';
-import Main from '../../pages/Main/Main';
 import Vacancy from '../../pages/Vacancy/Vacancy';
 import Candidates from '../../pages/Candidates/Candidates';
 import Candidate from '../../pages/Candidate/Candidate';
 import Favorites from '../../pages/Favorites/Favorites';
 import Login from '../../pages/Login/Login';
+import ProtectedRouteElement from '../ProtectedRoute/ProtectedRoute';
 
 import testResume from '../../utils/testResume.json';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  const [isLogged, setIsLogged] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  function logOut() {
-    localStorage.clear();
-
-    dispatch(clearUser());
-    dispatch(clearCandidates());
-
-    setIsLogged(false);
-  }
-
-  function searchCandidates() {
+  function searchCandidates(): void {
     const token = localStorage.getItem('token');
 
     if (token) {
@@ -43,44 +40,58 @@ function App() {
     }
   }
 
+  function logIn(username: string, password: string): void {
+    mainApi.signIn(username, password)
+      .then(({ auth_token }) => {
+        localStorage.setItem('token', auth_token);
+
+        mainApi.getUser(auth_token)
+          .then((user) => {
+            dispatch(addUser({ user }));
+
+            setIsLoggedIn(true);
+
+            searchCandidates();
+
+            navigate('/', { replace: true });
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function logOut(): void {
+    localStorage.clear();
+
+    dispatch(clearUser());
+    dispatch(clearCandidates());
+
+    setIsLoggedIn(false);
+
+    navigate('/login', { replace: true });
+  }
+
   useEffect(() => {
     dispatch(addCandidateInfo({ candidateInfo: testResume[0] }));
 
     const token = localStorage.getItem('token');
 
     if (token) {
-      setIsLogged(true);
+      const path = location.pathname;
+
       mainApi.getUser(token)
         .then((user) => {
           dispatch(addUser({ user }));
-        })
-        .catch((err) => console.log(err));
 
-      searchCandidates();
-    } else {
-      mainApi.signIn('front_test', 'Hakaton8front')
-        .then(({ auth_token }) => {
-          setIsLogged(true);
-
-          localStorage.setItem('token', auth_token);
-          mainApi.getUser(auth_token)
-            .then((user) => {
-              dispatch(addUser({ user }));
-            })
-            .catch((err) => console.log(err));
-
-          // mainApi.getCandidates(token.auth_token)
-          //   .then((candidate) => console.log(candidate));
-          // mainApi.getCandidateExperience(token.auth_token)
-          //   .then((candidate) => console.log(candidate));
-          // mainApi.getCandidateEducation(token.auth_token)
-          //   .then((candidate) => console.log(candidate));
-          // mainApi.logOut(token.auth_token)
-          //   .then((mes) => console.log(mes));
+          setIsLoggedIn(true);
 
           searchCandidates();
+
+          navigate(path, { replace: true });
         })
         .catch((err) => console.log(err));
+    } else {
+      navigate('/login', { replace: true });
     }
 
     return undefined;
@@ -89,31 +100,60 @@ function App() {
 
   return (
     <div className="app">
-      <Sidebar onLogOut={() => logOut()} />
       <Routes>
         <Route
           path="/"
-          element={(<Main />)}
-        />
-        <Route
-          path="/vacancy"
-          element={(<Vacancy />)}
+          element={(
+            <>
+              <Sidebar onLogOut={() => logOut()} />
+              <ProtectedRouteElement
+                element={<Vacancy />}
+                isLoggedIn={isLoggedIn}
+              />
+            </>
+          )}
         />
         <Route
           path="/candidates"
-          element={(<Candidates />)}
-        />
-        <Route
-          path="/candidates/:id"
-          element={(<Candidate />)}
+          element={(
+            <>
+              <Sidebar onLogOut={() => logOut()} />
+              <ProtectedRouteElement
+                element={<Candidates />}
+                isLoggedIn={isLoggedIn}
+              />
+            </>
+          )}
         />
         <Route
           path="/favorites"
-          element={(<Favorites />)}
+          element={(
+            <>
+              <Sidebar onLogOut={() => logOut()} />
+              <ProtectedRouteElement
+                element={<Favorites />}
+                isLoggedIn={isLoggedIn}
+              />
+            </>
+          )}
+        />
+        <Route
+          path="/candidates/:id"
+          element={(
+            <>
+              <Sidebar onLogOut={() => logOut()} />
+              <Candidate />
+            </>
+          )}
         />
         <Route
           path="/login"
-          element={(<Login />)}
+          element={(
+            <Login
+              // eslint-disable-next-line react/jsx-no-bind
+              logIn={logIn}
+            />
+          )}
         />
       </Routes>
     </div>
