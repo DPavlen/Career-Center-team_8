7d1a5d312a05c4009631f8f3b7bd585e6c5a0cd6
Pavlen <pavlen@192.168.1.84>
2023-10-27 20:48:55 +0300

    Edit Urls plural & csv detailed

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -3,10 +3,11 @@ from rest_framework import routers
 
 from users.views import CustomUserViewSet
 from api.v1.views import (
-    ExperienceDetailedViewSet,
-    EducationViewSet,
+    # ExperienceDetailedViewSet,
+    # EducationViewSet,
     CandidateViewSet,
-    ShortCandidateViewSet)
+    ShortCandidateViewSet
+    )
 
 
 app_name = "api.v1"
@@ -15,11 +16,11 @@ app_name = "api.v1"
 router = routers.DefaultRouter()
 
 router.register("users", CustomUserViewSet, "users")
-router.register("short_candidate", ShortCandidateViewSet, "short_candidate")
-router.register("candidate", CandidateViewSet, "candidate")
-router.register("experience_detailed", 
-                ExperienceDetailedViewSet, "experience_detailed")
-router.register("education", EducationViewSet, "education")
+router.register("short_candidates", ShortCandidateViewSet, "short_candidates")
+router.register("candidates", CandidateViewSet, "candidates")
+# router.register("experience_detailed", 
+#                 ExperienceDetailedViewSet, "experience_detailed")
+# router.register("education", EducationViewSet, "education")
 
 urlpatterns = [
     path("v1/", include(router.urls)),
