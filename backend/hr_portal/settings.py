96361a6f682a3469fab32c87fb52679f78dba3a0
Pavlen <pavlen@192.168.1.63>
2023-10-30 20:08:30 +0300

    settings corsheaders

--- backend/hr_portal/settings.py
+++ backend/hr_portal/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "users.apps.UsersConfig",
     "rest_framework",
     "rest_framework.authtoken",
@@ -26,18 +27,19 @@ INSTALLED_APPS = [
     "vacancies.apps.VacanciesConfig",
     "api.v1.apps.ApiConfig",
     "core",
-    "corsheaders",
+    # "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
