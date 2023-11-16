9525da0e2cd5b1b9b12cd93aff8e94c20c661975
Pavlen <pavlen@192.168.1.64>
2023-11-16 22:22:08 +0300

    Del comments

--- backend/hr_portal/urls.py
+++ backend/hr_portal/urls.py
@@ -47,15 +47,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-    
-
-
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('api.v1.urls')),
-#     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-# ]
